7554514e7ae4721c281148b3d7d91ec8fb90a287
Pradum <satendarpradum@gmail.com>
2024-07-30 00:13:15 +0530

    Frontend

---
+++ 1st/1stProject/Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Home from './components/Home'
+import { useState } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Signup/>}/>
+      <Route path='/register' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/home' element={<Home />}/>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
